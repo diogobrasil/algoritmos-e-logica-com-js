e87d46657e5fc0d72745b688d5e79f1954010b4e
diogo <diogobrasildasilva@gmail.com>
2024-06-12 19:31:47 -0300

    Aula 1: usando prompt e alert para obter o nome do usuário e apresentar na tela.

---
+++ script.js
@@ -0,0 +1,6 @@
+/*
+  Resolva o seguite problema: 
+    pergunte o nome do usuário e escreva a mensagem "Olá, [nome do usuário]"
+*/
+let name = prompt("Qual o seu nome ?");
+alert ("Olá, " + name);
